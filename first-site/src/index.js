da4f93e383409d9dcd867762a04497135783d94c
Dinujaya Sandaruwan <info.dinujaya@gmail.com>
2023-03-10 13:34:16 +0530

    Before Exercise

--- first-site/src/index.js
+++ first-site/src/index.js
@@ -1,17 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import App from "./App";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
-import Counter from "./components/counter";
 
 // import Counter from "./components/demoCounter";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<Counter />
+		<App />
 	</React.StrictMode>
 );
 
